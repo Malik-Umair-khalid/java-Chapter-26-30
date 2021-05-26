381807e2abdb116726bf13afded2ba4b2dc9b86b
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-05-26 23:24:01 +0500

    completed

---
+++ app.js
@@ -0,0 +1,84 @@
+// //********************* Q : 1 ********************** */
+// var userInput = +prompt("Please Write A Positive Integer")
+// document.write('<h2>' + "Number = " + userInput + "</h2>")
+// document.write('<h2>' + "Round Of = " + Math.round(userInput) + "</h2>")
+// document.write('<h2>' + "Ceal = " + Math.ceil(userInput) + "</h2>")
+// document.write('<h2>' + "Floor = " + Math.floor(userInput) + "</h2>")
+
+
+// //********************* Q : 2 ********************** */
+
+// var userInput1 = +prompt("please Wrile a -ive Number")
+
+//   if(userInput1 > 0){
+//       alert("Please Wrie a Negatine Number")
+//   }
+//   else if(userInput1 < 0){
+//       document.write('<h2>' + "Number = " + userInput1 + "</h2>")
+//     document.write('<h2>' + "Round Of = " + Math.round(userInput1) + "</h2>")
+//     document.write('<h2>' + "Ceal = " + Math.ceil(userInput1) + "</h2>")
+//     document.write('<h2>' + "Floor = " + Math.floor(userInput1) + "</h2>")
+//   }
+
+// //********************* Q : 3 ********************** */
+
+// var userInput3 = +prompt("Please Write A Number")
+// alert(Math.abs(userInput3))
+
+// //********************* Q : 4 ********************** */
+
+//  var randomNum = Math.random();
+//  var dice = (randomNum * 6) + 1;
+//  var dice1 = Math.floor(dice);
+//  alert(dice1)
+
+// //********************* Q : 5 ********************** */
+
+// var player1 =   prompt("Write Player One Name")
+// var player2 =   prompt("Write Player Two Name")
+// var player1Choise   =   prompt("What is Player One Choise")
+// if(player1Choise.toLowerCase() === "heads"){
+//     var player2Choise = "tails"
+// }
+// else if(player1Choise.toLowerCase() === "tails"){
+//     var player2Choise = "heads"
+// }
+// else{
+//     alert("Invaled Choise !")
+// }
+// // console.log(player2Choise)
+// var toss = Math.round(Math.random())
+// console.log(toss)
+
+// if(toss === 0){
+//     alert(player1 + " Choise :" + player1Choise + " Won ")
+// } 
+// else if (toss === 1){
+//     alert(player2 + " Choise :" + player2Choise + " Won ")
+// } 
+
+// //********************* Q : 6 ********************** */
+
+//  var randomNum = Math.random();
+//  var dice = (randomNum * 100) + 1;
+//  var dice1 = Math.floor(dice);
+//  alert(dice1)
+
+// //********************* Q : 8 ********************** */
+
+//  var guess = +prompt("Guess A No From 1-10") 
+//  var randomNum = Math.random();
+//  var randomNum10 = Math.round(randomNum * 10) + 1;
+
+// if(guess === randomNum10){
+//     alert("The Number is :" + randomNum10 + " and Your Guess Is : " + guess + " Congrates You Won") 
+// }
+// else{
+//     alert("The Number is :" + randomNum10 + " and Your Guess Is : " + guess + " Try Again") 
+// }
+
+// //********************* Q : 9 ********************** */
+
+//Sir Weight wala sawal samajh nai aya
+
+
